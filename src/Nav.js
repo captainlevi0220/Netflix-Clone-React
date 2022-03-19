80bdc7dda0a32a4f9d4308cfb603d57f8756de72
captainlevi <captainlevi0220@protonmail.com>
2022-03-18 20:31:27 -0400

    navbar css and fade transition complete

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Nav.css'
 
 const Nav = () => {
+  const [show, handleShow] = useState(false)
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShow(true)
+      } else handleShow(false)
+    })
+    return () => {
+      window.removeEventListener('scroll')
+    }
+  }, [])
   return (
-    <div className='nav'>
+    <div className={`nav ${show && 'nav__black'}`}>
       <img
         src='https://i.ibb.co/GRDq2qt/Netflix-Logo-1.png'
         alt='Netflix Logo'
